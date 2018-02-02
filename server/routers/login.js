fa498aa72568813c05859437b56a658d9af39a06
Elias Hantula <ehantula@indiana.edu>
2018-02-02 18:56:49 -0500

    up

---
+++ server/routers/login.js
@@ -0,0 +1,30 @@
+
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+let models = require('./../models');
+let User = mongoose.model('User');
+
+// ----------------------------------------
+// Passport
+// ----------------------------------------
+
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+
+// ----------------------------------------
+// Routes for /login
+// ----------------------------------------
+
+
+router.post('/', passport.authenticate('local'), function(req, res) {
+  console.log('callback called');
+  // If this function gets called, authentication was successful.
+  // `req.user` contains the authenticated user.
+  return res.json(req.user);
+});
+
+
+module.exports = router;
